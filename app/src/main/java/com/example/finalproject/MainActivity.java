f473fc3fb8171d287566b0ae7a09d2067ec98de6
ckamin <clk43@psu.edu>
2018-03-14 19:41:30 -0400

    code?

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -2,14 +2,60 @@ package com.example.finalproject;
 
 //    tools:context="com.example.finalproject.MainActivity"
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(this);
     }
-}
+
+    public void showDialog(View view){
+        AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+        alertDialog.setTitle("Alert Dialog Title");
+        alertDialog.setMessage("Here is android alert dialog message");
+        // Alert dialog button
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Alert dialog action goes here
+                        // onClick button code here
+                        dialog.dismiss();// use dismiss to cancel alert dialog
+                    }
+                });
+        alertDialog.show();
+    }
+
+    public void onClick(View v) {
+        Toast toast = Toast.makeText(getApplicationContext(), "Retrieving location...", Toast.LENGTH_LONG);
+        toast.show();
+
+        AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+        alertDialog.setTitle("Alert Dialog Title");
+        alertDialog.setMessage("Here is android alert dialog message");
+        // Alert dialog button
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Alert dialog action goes here
+                        // onClick button code here
+                        dialog.dismiss();// use dismiss to cancel alert dialog
+                    }
+                });
+        alertDialog.show();
+    }
+
+
+    }
+
+
--- app/src/main/java/com/example/finalproject/MainPage.java
+++ app/src/main/java/com/example/finalproject/MainPage.java
@@ -1,6 +1,9 @@
 package com.example.finalproject;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainPage extends AppCompatActivity {
@@ -9,5 +12,6 @@ public class MainPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+
     }
 }
